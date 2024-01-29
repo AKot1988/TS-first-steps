bf731d918b39fb924e1285a2f7d675892677bd4b
AKot1988 <kulibabenko@ukr.net>
2024-01-29 22:54:10 +0200

    create vite project call 29 jan 2024

---
+++ Call-TS-jan-29-2024/src/main.ts
@@ -0,0 +1,34 @@
+// const STATUS = {
+//   ERROR: 'error',
+//   SUCSESS: 'success',
+//   LOADING: 'loading',
+// }
+// type Status = keyof typeof STATUS
+
+// enum METHOD {
+//   POST = 'POST',
+//   GET = 'GET',
+//   PUT = 'PUT',
+//   DELETE = 'DELETE',
+// }
+// type  Method = keyof typeof METHOD
+
+// type Options = {
+//   method: Method
+//   body: any
+// }
+
+// function fetcher(url: string, options: Options): [boolean, string, any[]] {
+//   let loading: boolean = false
+//   let error: string = ''
+//   let data: any[] = []
+ 
+//   fetch(url, options)
+
+
+//   return [loading, error, data]
+// }
+
+// function fetcherResoler() {
+  
+// }
